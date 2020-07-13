98cab41204e7b1b2ee919ec03b8ab460e68da1c6
Daan Ruijter <daan_ruijter@hotmail.com>
2020-07-13 11:05:57 +0200

    created color output in terminal

--- MultiPlayerTester.java
+++ MultiPlayerTester.java
@@ -3,7 +3,9 @@ import java.util.Random;
 import java.util.ArrayList;
 
 
+
 class MultiPlayerTester {     
+
    
             int position;
      int numDiceEyes;
@@ -16,6 +18,7 @@ this.numDiceEyes = numDiceEyes;
 
   public static void main(String[] args){
 
+
    
         int position = 0;
      int numDiceEyes= 0;
@@ -27,15 +30,17 @@ this.numDiceEyes = numDiceEyes;
   //ask user to throw the dice//
    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
-   System.out.println("Welcome to the ancient Dutch game of Ganzenbord\n");
 
-   System.out.println("With how many players do you want to play? Enter a number" );
+   System.out.println(ConsoleColors.RED + "Welcome to the ancient Dutch game of Ganzenbord\n");
+   
+
+   System.out.println(ConsoleColors.RESET + "With how many players do you want to play? Enter a number\n" );
 int numPlayers = myObj.nextInt();  // Read user input
 
     //loop numPlayer times and create player names and or player objects //
    for(int i=0;i<(numPlayers);i++){
 
-        System.out.println(i);
+      
 
         // MultiPlayerTester player
  
@@ -43,15 +48,13 @@ int numPlayers = myObj.nextInt();  // Read user input
        
          // Read user input
   
-      	System.out.println("Enter the name of player " + (i+1));
+      	System.out.println("Enter the name of player " + (i+1)+"\n");
          
            String playerName = myObj.next();
 
        
     playersArray.add(playerName);
 
-
-      System.out.println("We play with " + numPlayers + " players");
       
    }
 
@@ -65,18 +68,18 @@ int numPlayers = myObj.nextInt();  // Read user input
 
 
 // while(position < 63){
-    System.out.println(playersArray.get(i) + ", throw your dice by pressing enter");
+    System.out.println(ConsoleColors.RESET + playersArray.get(i) + ", throw your dice by pressing enter\n");
      String throwDice = myObj.nextLine();  // Read user input
   Random random = new Random();
      numDiceEyes = random.nextInt(6) +1;
 // myObj.next();
-     System.out.println(numDiceEyes);
+     System.out.println(ConsoleColors.BLUE + "You threw " + numDiceEyes + playersArray.get(i));
 
        positionArray.add(numDiceEyes);
 
           //bridge: go to position 12//
       if (positionArray.get(i)==6){
-          System.out.println("Lucky you, " + playersArray.get(i) + "! " + "You are on the bridge and can move on to position 12!");
+          System.out.println(ConsoleColors.RESET + "Lucky you, " + playersArray.get(i) + "! " + "You are on the bridge and can move on to position 12!\n");
          positionArray.set(i, 12);
       }
         }
@@ -84,11 +87,11 @@ outer:
  for(int j=0;2<63; j++){  
    
    for(int i=0;i<playersArray.size(); i++){   
-       System.out.println(positionArray);
+       System.out.println(ConsoleColors.RESET + positionArray);
        System.out.println(positionArray.get(i));
      
        
-  System.out.println(playersArray.get(i) + ", throw your dice by pressing enter"); 
+  System.out.println(ConsoleColors.RESET + playersArray.get(i) + ", throw your dice by pressing enter\n"); 
 
 
 
@@ -97,7 +100,7 @@ outer:
   Random random = new Random();
      numDiceEyes = random.nextInt(6) +1;
 
-     System.out.println(playersArray.get(i) + ", you threw " + numDiceEyes + " and you are on field " + (positionArray.get(i) + numDiceEyes) + ". No worries!");
+     System.out.println(ConsoleColors.BLUE + playersArray.get(i) + ", you threw " + numDiceEyes + " and you are on field " + (positionArray.get(i) + numDiceEyes) + ". No worries!\n");
 
    
 
