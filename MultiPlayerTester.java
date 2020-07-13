5939ab3aad7db8072314361f645876bd1ddcd839
Daan Ruijter <daan_ruijter@hotmail.com>
2020-07-13 12:29:00 +0200

    trying to figure out how to make a player skip his turn

--- MultiPlayerTester.java
+++ MultiPlayerTester.java
@@ -73,18 +73,20 @@ int numPlayers = myObj.nextInt();  // Read user input
   Random random = new Random();
      numDiceEyes = random.nextInt(6) +1;
 // myObj.next();
-     System.out.println(ConsoleColors.BLUE + "You threw " + numDiceEyes + playersArray.get(i));
+     System.out.println(ConsoleColors.BLUE + "You threw " + numDiceEyes + ", " + playersArray.get(i));
 
        positionArray.add(numDiceEyes);
 
           //bridge: go to position 12//
       if (positionArray.get(i)==6){
-          System.out.println(ConsoleColors.RESET + "Lucky you, " + playersArray.get(i) + "! " + "You are on the bridge and can move on to position 12!\n");
+          System.out.println(ConsoleColors.GREEN + "Lucky you, " + playersArray.get(i) + "! " + "You are on the bridge and can move on to position 12!\n");
          positionArray.set(i, 12);
       }
         }
 outer:
  for(int j=0;2<63; j++){  
+
+    System.out.println(ConsoleColors.RED + j + "the value of j");
    
    for(int i=0;i<playersArray.size(); i++){   
        System.out.println(ConsoleColors.RESET + positionArray);
@@ -102,31 +104,62 @@ outer:
 
      System.out.println(ConsoleColors.BLUE + playersArray.get(i) + ", you threw " + numDiceEyes + " and you are on field " + (positionArray.get(i) + numDiceEyes) + ". No worries!\n");
 
-   
+             //goose that hit position 19 need to skip one turn
+        if (positionArray.get(i)==19){
+             
+            System.out.println(ConsoleColors.YELLOW + "Skip a turn, " +  playersArray.get(i));          
+            continue;    
+            
+              }
+ 
 
-      //update position of player i
+      //update position of player i after throwing
        positionArray.set(i, (positionArray.get(i) + numDiceEyes));
 
-
-      
-
-
-
-      //for goose that needs to breed, the game is over
-        if (positionArray.get(i)==23){
-             
-            System.out.println("You have to brood eggs, so you can't play anymore!, " +  playersArray.get(i) + " GAME OVER!");          
-            playersArray.remove(i);
-            positionArray.remove(i);
-          }
-
-
-    
-         if(positionArray.get(i)>63){
-          System.out.println(playersArray.get(i) + ", you won!!!!! Your position is: " + positionArray.get(i) );
+             if(positionArray.get(i)==63){
+          System.out.println(ConsoleColors.PURPLE + playersArray.get(i) + ", YOU WON!!!!! Your position is: " + positionArray.get(i) );
         
          break outer;
        }
+
+      //goose have throw the right number to get on position 63 and to win
+      if(positionArray.get(i)>63){
+          System.out.println(ConsoleColors.RED + playersArray.get(i) + ", You threw " + numDiceEyes + " which is " + (positionArray.get(i) - 63) + " too much! Your position is: " + (63 -(positionArray.get(i) - 63)));
+          positionArray.set(i,(63 -(positionArray.get(i) - 63)));
+       }
+
+           //goose that hit the tens get extra steps, equal to what they threw
+     if(positionArray.get(i) == 10 || positionArray.get(i) == 20 || positionArray.get(i) == 30 || positionArray.get(i) == 40 || positionArray.get(i) == 50 || positionArray.get(i) == 60){
+            positionArray.set(i, (positionArray.get(i)+numDiceEyes));
+            
+          System.out.println(ConsoleColors.GREEN + " You threw " + numDiceEyes + ", " + playersArray.get(i) + ", and you are on field " + (positionArray.get(i)-numDiceEyes) + ": Bonus steps! Move on to field " + positionArray.get(i) + "!" );
+          
+           
+       }
+
+
+      //for goose that needs to breed at position 23, the game is over
+      //   if (positionArray.get(i)==23){
+             
+      //       System.out.println(ConsoleColors.GREEN + "You have to brood eggs, so you can't play anymore!, " +  playersArray.get(i) + " GAME OVER!");          
+      //       playersArray.remove(i);
+      //       positionArray.remove(i);
+      //     }
+
+    //goose that get trapped in a maze at position 42 need to step back 3 positions
+        if (positionArray.get(i)==42){
+             
+            System.out.println(ConsoleColors.RED + "You gut stuck in a maze on position 42! Go back to field 39, " +  playersArray.get(i) + "!");          
+            positionArray.set(i, 39);
+                      }
+    
+        //goose that are dead at position 58 need to start over
+        if (positionArray.get(i)==58){
+             
+            System.out.println(ConsoleColors.RED + "You didn't survive a sad deadly accident on position 58, go back to start, " +  playersArray.get(i) + "!");          
+            positionArray.set(i, 0);
+                      }
+   
 
 
         }
